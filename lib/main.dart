5e7d16834b45d5f951416367313ed7b06e8995c9
cristiancisneros <cristianj2@icloud.com>
2021-04-12 21:46:02 -0700

    water project login

--- lib/main.dart
+++ lib/main.dart
@@ -1,59 +1,57 @@
 import 'package:flutter/material.dart';
-
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  String password = '';
+  bool isPasswordVisible = false;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-
-        primarySwatch: Colors.blue,
-
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-
-
-
-  @override
-  Widget build(BuildContext context) {
-
-    return Scaffold(
-      appBar: AppBar(
-
-        title: Text(widget.title),
-      ),
-      body: Center(
-
-        child: Column(
-
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-
-          ],
+      home: Scaffold(
+        body: Center(
+          child: ListView(
+            padding: EdgeInsets.all(32),
+            children: [
+              TextField(
+                decoration: InputDecoration(
+                  hintText: 'name@example.com',
+                  labelText: 'Email',
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              TextField(
+                onChanged: (value) => setState(()=> this.password = value),
+                onSubmitted:  (value) => setState(()=> this.password = value),
+                decoration: InputDecoration(
+                  labelText: 'Password',
+                  border: OutlineInputBorder(),
+                  errorText: 'Password is wrong',
+                  suffixIcon: IconButton(
+                    icon: isPasswordVisible
+                        ? Icon(Icons.visibility_off)
+                        : Icon(Icons.visibility),
+                    onPressed: () =>
+                      setState(()=> isPasswordVisible = !isPasswordVisible),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 24.0,),
+              ElevatedButton(
+                onPressed: (){},
+                child: Text('Submit'),
+              )
+            ],
+          ),
         ),
       ),
-
     );
   }
 }
